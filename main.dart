a6c741a0730564881847e5bb73235c7a07f3bbe8
Lucas Paiva-Bernard <104046971+lpaiva04@users.noreply.github.com>
2022-11-10 01:37:01 -0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,22 +1,29 @@
-
 import 'package:flutter/material.dart';
 
-//The main function is the starting point for all our Flutter apps
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png')
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello')
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
